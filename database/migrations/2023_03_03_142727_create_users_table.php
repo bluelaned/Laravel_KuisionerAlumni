1be695db7e73e8b3eb94f1c966f853307ac317e5
bluelaned <43365136+bluelaned@users.noreply.github.com>
2023-08-31 01:41:48 +0700

    remove role migration 'mahasiswa'

--- database/migrations/2023_03_03_142727_create_users_table.php
+++ database/migrations/2023_03_03_142727_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->enum('role', ['admin', 'alumni', 'mahasiswa'])->default('mahasiswa');
+            $table->enum('role', ['admin', 'alumni'])->default('admin');
             $table->integer('npm');
             $table->string('name');
             $table->string('email')->unique();
